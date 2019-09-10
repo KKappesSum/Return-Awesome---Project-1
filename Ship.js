72071b3b4b4b856243d157eed0a04e049ee9b489
Connie <constanceli@ku.edu>
2019-09-10 13:27:25 -0500

    create Ship class
    write constructor.  all class properties are currently public.

---
+++ Ship.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export class Ship {
+	constructor(size) {
+		this.size = size;
+		this.afloat = true;
+		this.numHits = 0;
+		this.coords = newArray(size);
+	}
+
+	/* TODO: setters & getters, if private vars are needed */
+
+
+}
