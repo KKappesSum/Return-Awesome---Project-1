bd7532b18ee3ce2589528ac7652961bb7d667942
Connie <constanceli@ku.edu>
2019-09-14 00:17:34 -0500

    implement private class fields & add getSize member method
    start adding documentation using documentation.js

--- Ship.js
+++ Ship.js
@@ -1,14 +1,25 @@
-'use strict';
+/** Class representing a Ship. */
+class Ship {
+	#size;
+	#afloat;
+	#numHits;
+	#coords;
 
-export class Ship {
+	/**
+	 * Create a Ship object.
+	 * @param {number} size - The size (i.e. length) of the Ship.
+	 */
 	constructor(size) {
-		this.size = size;
-		this.afloat = true;
-		this.numHits = 0;
-		this.coords = newArray(size);
+		this.#size = size;
+		this.#afloat = true;
+		this.#numHits = 0;
+		this.#coords = newArray(size);
 	}
 
-	/* TODO: setters & getters, if private vars are needed */
+	/* TODO: setters & getters */
 
+	getSize() {
+		return Ship.#size;
+	}
 
 }
