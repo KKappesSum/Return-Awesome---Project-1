2ecc7bdb26e81532c5791889f15e99c202ff9bce
archana <archana2359@gmail.com>
2019-09-14 14:52:30 -0500

    Features added : sets plaer names or default names, validates shipNum. Functions added: setShipCount(), setPlayerNames()

--- gui.js
+++ gui.js
@@ -3,15 +3,34 @@
  * @param {number} input any number
  * @returns {number} that number, plus one.
  */
-function othername() {
+function setPlayerNames() 
+{
     var p1 = document.getElementById("player1").value;
     var p2 = document.getElementById("player2").value;
-    var numShips= document.getElementById("ships").value;
-    alert(p1);
-    alert(p2);
-	alert(numShips);
-	while (numShips > 5 || numShips < 1) 
+    if(!(document.getElementById("player1") && document.getElementById("player1").value))
 	{
-  		
-	}
+    	p1 = document.getElementById("player1").placeholder;
+	} 
+	if(!(document.getElementById("player2") && document.getElementById("player2").value))
+	{
+		p2 = document.getElementById("player2").placeholder;
+	} 
+
+    document.getElementById("names").innerHTML =
+    	"These are your names " + p1 + " and " + p2;
+}
+
+function setShipCount() 
+{
+	//get input from user in pop up
+	var numShips = prompt("Please enter number of ships between 1 and 5: ");
+
+	//validate that number of ships is between 1 and 5/
+	//prompt until you recieve a valid input
+  	while(numShips > 5 || numShips < 1 || numShips == null)
+  	{
+  		var numShips = prompt("Please enter number of ships between 1 and 5: ");
+  	}
+  	document.getElementById("ships").innerHTML =
+    	"You have chosen " + numShips + " ships ";
 }
