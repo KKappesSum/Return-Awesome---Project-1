7d104b0896867b495abb5bdc060528f8afc3c5b8
Malena <msch179@ku.edu>
2019-09-14 12:07:08 -0500

    feat: made the ship/firing map and other variables private

--- Admiral.js
+++ Admiral.js
@@ -5,28 +5,33 @@ admiral has numShips = number of ships
 Do you guys want the name to be passed in as well??
 */
 class Admiral {
+  _numShips;
+  _firingMap;
+  _shipMap;
+  _fleet;
+  name;
   constructor(num, pName) {
-    this.numShips = num;
-    this.firingMap = 0;
-    this.shipMap = 0;
-    this.fleet = 0;
+    this._numShips = num;
+    this._firingMap = 0;
+    this._shipMap = 0;
+    this._fleet = 0;
     this.name = pName;
   }
 
   getNumShips() {
-    return this.numShips;
+    return this._numShips;
   }
 
   getFiringMap() {
-    return this.firingMap;
+    return this._firingMap;
   }
 
   getShipMap() {
-    return this.shipMap;
+    return this._shipMap;
   }
 
   getFleet() {
-    return this.fleet;
+    return this._fleet;
   }
 
   getName() {
@@ -34,19 +39,19 @@ class Admiral {
   }
 
   setNumShips(x) {
-    this.numShips = x;
+    this._numShips = x;
   }
 
   setFiringMap(x) {
-    this.firingMap = x;
+    this._firingMap = x;
   }
 
   setFleet(x) {
-    this.fleet = x;
+    this._fleet = x;
   }
 
   setShipMap(x) {
-    this.shipMap = x;
+    this._shipMap = x;
   }
 
   setName(x) {
