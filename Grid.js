b6a726255042f332dfa99e8d8fb9c91aa6ff5634
Kyle Kappes-Sum <kylekappessum@gmail.com>
2019-09-14 14:06:37 -0500

    feat: Added populateGrid function
    populateGrid() is  the function for filling in a grid based on a ships coordinate array

--- Grid.js
+++ Grid.js
@@ -1,13 +1,29 @@
 class Grid{   
     constructor(size){
         let conf = new Config();
-        this.arr = new Array(size);
+        this.#arr = new Array(size);
         for(let i = 0; i < size; i++){
             arr[i] = new Array(size);
             for(let j = 0; j < size; j++){
                 arr[i][j] = conf.oceanTypes.properties["WATER"].code;
             }
         }
+
+        function createShip(location){
+            
+        }
     }
-//old
+
+    populateGrid(locationArr){
+        for(let i = 0; i < locationArr.length; i++){
+            #arr[i] = conf.oceanTypes.properties["SHIP"].code;
+        }        
+    }
+
+    updateCell(location){
+        this.#isHit = false;
+        return this.#isHit;
+    }
+
+    
 }
