18e9804ae57da157c67aed728bde0813b759bd9b
Connie <constanceli@ku.edu>
2019-09-16 14:20:47 -0500

    feat: code to test creation of Admirals and Ships
    no ship placement, no grid updates

--- testers/shipTester.js
+++ testers/shipTester.js
@@ -1,1 +1,10 @@
 // TODO:test Ship & Admiral
+
+//let admA1 = new Admiral(1, "A1");
+//console.log(admA1);
+
+for(let i = 1; i <= 5; i++)
+{
+	let adm = new Admiral(i, "A" + i);
+	console.log(adm);
+}
