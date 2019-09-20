994531dfb65e849ae0c4e772ff1a8d3350fcf170
Connie <constanceli@ku.edu>
2019-09-20 13:17:00 -0500

    test: Exec, constructor & object fields
    init: execTester.js
    edit test page

---
+++ testers/execTester.js
@@ -0,0 +1,8 @@
+// create an Exec object
+let exec1 = new Exec("p1", "p2", 5);
+
+// check object contents
+console.log("player turn: " + exec1.getPlayerTurn());
+console.log("shipcount: " + exec1.m_shipCount);
+console.log("admir1: " + exec1.admir1.getName());
+console.log("admir2: " + exec1.admir2.getName());
