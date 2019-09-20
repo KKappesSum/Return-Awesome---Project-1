cbb23ef66d435a9e868a50f77ee9c9d565bc6297
Connie <constanceli@ku.edu>
2019-09-19 20:00:40 -0500

    test: setters & getters in Admiral
    and include admiral tester scripts in test page

---
+++ testers/admiralTester.js
@@ -0,0 +1,9 @@
+// create admirals
+let adm1 = new Admiral(1, "adm1");
+let adm2 = new Admiral(1, "adm2");
+
+// test setters/getters with original stats
+console.log("# ships: " + this.getNumShips());
+console.log("board: " + this.getBoard());
+console.log("fleet: " + this.getFleet());
+console.log("name: " + this.getName());
