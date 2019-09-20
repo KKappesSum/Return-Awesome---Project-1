cd96ef49700111eedd0bab9b3b0b95c82ce93b51
Connie <constanceli@ku.edu>
2019-09-19 21:28:01 -0500

    test: code to test getters cont'd

--- testers/admiralTester.js
+++ testers/admiralTester.js
@@ -1,9 +1,15 @@
 // create admirals
-let adm1 = new Admiral(1, "adm1");
-let adm2 = new Admiral(1, "adm2");
+let adm1 = new Admiral(1, "admiral 1");
+let adm2 = new Admiral(1, "admiral 2");
 
-// test setters/getters with original stats
-console.log("# ships: " + this.getNumShips());
-console.log("board: " + this.getBoard());
-console.log("fleet: " + this.getFleet());
-console.log("name: " + this.getName());
+// test getters with original stats
+console.log("# ships: " + adm1.getNumShips());
+console.log("board: " + adm1.getBoard());
+console.log("fleet: " + adm1.getFleet());
+console.log("name: " + adm1.getName());
+
+console.log("# ships: " + adm2.getNumShips());
+console.log("board: " + adm2.getBoard());
+console.log("fleet: " + adm2.getFleet());
+console.log("name: " + adm2.getName());
+// 
