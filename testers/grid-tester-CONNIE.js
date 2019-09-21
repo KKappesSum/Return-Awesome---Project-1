b1a664f235da180f332d4c9db2b99e57f8c41fea
Connie <constanceli@ku.edu>
2019-09-21 14:13:24 -0500

    test (Grid): getters

--- testers/grid-tester-CONNIE.js
+++ testers/grid-tester-CONNIE.js
@@ -3,8 +3,14 @@
 // constructor
 let config = new Config();
 let adm = new Admiral(5, "ADMIRAL SPOCK");
-console.log("Board: " + adm.getBoard());
 
+// getters
+console.log(adm.getBoard().getArr());
+console.log(adm.getBoard().getCell("1:1"));
+console.log(adm.getBoard().getRow(0));
+console.log(adm.getBoard().getCol(0));
+
+/*
 // place all 5 ships vertically, aligned to top row, from left to right
 let row = 1;
 let col = 1;
@@ -13,3 +19,5 @@ while(numShips > 0) {
 	adm.assignCoords((row + ":" + col), numShips, false, "ship1");
 }
 console.log(adm.getBoard());
+console.log(adm.getBoard().getCell("1:1"));
+*/
